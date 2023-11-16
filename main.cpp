2fc83443e52b1b7ef4547408189b9e3615430d26
Gabo-Puck <melkorgodo@gmail.com>
2023-11-16 00:00:00 -0600

    añadido creacion y lista de tareas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <string>
 #include <iostream>
+#include <vector>
+
 struct Task
 {
-    std::string name[100];
+    std::string name;
 };
+
+std::vector<Task> tasks;
+
+Task addTask(Task task)
+{
+    tasks.push_back(task);
+    return task;
+}
+
+Task readTask()
+{
+    Task newTask;
+    std::cout << "Write the task name: " << std::endl;
+    std::getline(std::cin >> std::ws, newTask.name);
+    return newTask;
+}
+
+void writeTasks()
+{
+    std::cout << std::endl
+              << std::endl
+              << "TASK LIST"
+              << std::endl
+              << std::endl;
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        Task task = tasks[i];
+        std::cout << "#" << i + 1 << " " << task.name << std::endl;
+    }
+    std::cout << std::endl
+              << std::endl
+              << std::endl
+              << std::endl;
+}
 
 void showMenu()
 {
@@ -20,10 +56,14 @@ void showMenu()
         switch (option)
         {
         case 1:
-            std::cout << "Create task" << std::endl;
+        {
+            // std::cout << "Create task" << std::endl;
+            Task newTask = readTask();
+            addTask(newTask);
             break;
+        }
         case 2:
-            std::cout << "Read task" << std::endl;
+            writeTasks();
             break;
         case 3:
             std::cout << "Update task" << std::endl;
